e07be8b64c0b346b128cd027ce93b82bb87b27ee
YANGEUNOK\82103 <tlawnsgh1@naver.com>
2023-02-22 10:30:29 +0900

    first

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping
+	public String Home() {
+		return "안녕하세요";
+	}
+}
